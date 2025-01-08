d87f46b4cb6c7d75ec663982a42d69c02ec1aacd
jhs512 <jangka512@gmail.com>
2025-01-08 12:07:07 +0900

    work

--- src/main/java/com/ll/spring_jwt_2025_01_07/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/domain/post/post/controller/ApiV1PostController.java
@@ -36,8 +36,6 @@ public class ApiV1PostController {
     public PostStatisticsResBody statistic() {
         Member actor = rq.getActor();
 
-        if (!actor.isAdmin()) throw new ServiceException("403-1", "관리자만 접근 가능합니다.");
-
         return new PostStatisticsResBody(
                 10,
                 10,
--- src/main/java/com/ll/spring_jwt_2025_01_07/global/security/SecurityConfig.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/global/security/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/api/*/members/login", "/api/*/members/join")
                                 .permitAll()
+                                .requestMatchers("/api/*/posts/statistics")
+                                .hasAuthority("ADMIN_ACTING")
                                 .anyRequest()
                                 .authenticated()
                 )
