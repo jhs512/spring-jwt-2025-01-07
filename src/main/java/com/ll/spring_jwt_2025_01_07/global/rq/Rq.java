981be97c27f02efd314f4134940e200f5edb8f1b
jhs512 <jangka512@gmail.com>
2025-01-09 12:23:02 +0900

    work

--- src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
@@ -78,4 +78,8 @@ public class Rq {
                 .findFirst()
                 .orElse(null);
     }
+
+    public void setHeader(String name, String value) {
+        resp.setHeader(name, value);
+    }
 }
